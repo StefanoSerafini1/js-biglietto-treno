b69df85cf04031ee79100803f33d9d8be2a9025c
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-14 18:32:23 +0200

    aggiunta scritte

--- myscript.js
+++ myscript.js
@@ -13,10 +13,10 @@ var prezzo40 = prezzoTratta * 40 /100;
 //ciclo di selezione con tre opzioni e stampa
 
 if (eta < 18) {
- document.getElementById('prezzo').innerHTML = prezzo20.toFixed(2);
+ document.getElementById('prezzo').innerHTML = prezzo20.toFixed(2) + '€  grazie allo sconto inserito del 20% per i minori di 18 anni';
   console.log('Il prezzo è ' + prezzo20.toFixed(2));
 } else if (eta > 65) {
-  document.getElementById('prezzo').innerHTML = prezzo40.toFixed(2);
+  document.getElementById('prezzo').innerHTML = prezzo40.toFixed(2) + '€  grazie allo sconto inserito del 40% per gli over 65';
    console.log('Il prezzo è ' + prezzo40.toFixed(2));
 } else {
   document.getElementById('prezzo').innerHTML = prezzoTratta.toFixed(2);
